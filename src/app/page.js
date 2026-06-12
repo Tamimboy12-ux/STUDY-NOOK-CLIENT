6da23d10f3b2495933a7d793484868575dcd5430
unknown <rtamim504@gmail.com>
2026-06-12 17:11:05 +0600

    Appended Testimonial section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/home/Banner";
+import Testimonials from "@/components/home/Testimonials";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Banner></Banner>
+      <Testimonials></Testimonials>
     </div>
   );
 }
