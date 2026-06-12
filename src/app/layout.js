175dee0dee903d54ccb3f60108dcefbebef34fe2
unknown <rtamim504@gmail.com>
2026-06-12 17:01:41 +0600

    add banner section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import { Toaster } from "react-hot-toast";
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
           {children}
         </main>
         <Footer></Footer>
-        {/* <Toaster/> */}
+        <Toaster/>
         </body>
     </html>
   );
